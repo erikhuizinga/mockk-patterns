37d518b60b895dd507e5b7b34399d28660857f51
Erik Huizinga <erik.huizinga@nedap.com>
2020-03-12 12:51:39 +0100

    🎉 Setup IntelliJ IDEA + Gradle + Kotlin project

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "com.github.erikhuizinga"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
