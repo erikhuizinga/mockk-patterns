ef771577f8e86150ffc48a3b895e2dd1faee8453
Erik Huizinga <erik.huizinga@nedap.com>
2020-03-14 15:38:26 +0100

    ✨ Add MockkUnmockExtension

--- junit5/src/main/kotlin/com/github/erikhuizinga/mockk/junit5/MockkExtensions.kt
+++ junit5/src/main/kotlin/com/github/erikhuizinga/mockk/junit5/MockkExtensions.kt
@@ -1,6 +1,8 @@
 package com.github.erikhuizinga.mockk.junit5
 
 import io.mockk.clearAllMocks
+import io.mockk.unmockkAll
+import org.junit.jupiter.api.extension.AfterAllCallback
 import org.junit.jupiter.api.extension.AfterEachCallback
 import org.junit.jupiter.api.extension.Extension
 import org.junit.jupiter.api.extension.ExtensionContext
@@ -10,4 +12,11 @@ import org.junit.jupiter.api.extension.ExtensionContext
  */
 class MockkClearExtension : AfterEachCallback {
     override fun afterEach(context: ExtensionContext?) = clearAllMocks()
+}
+
+/**
+ * Apply this [Extension] to unmock all Mockk mocks after the test class.
+ */
+class MockkUnmockExtension : AfterAllCallback {
+    override fun afterAll(context: ExtensionContext?) = unmockkAll()
 }
