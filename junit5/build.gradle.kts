6f7254835ae7fb74e7e8597375bee791a06151c3
Erik Huizinga <erik.huizinga@nedap.com>
2020-03-14 15:36:25 +0100

    🎉 Add module for mockk-junit5

---
+++ junit5/build.gradle.kts
@@ -0,0 +1,56 @@
+import com.jfrog.bintray.gradle.BintrayExtension
+
+plugins {
+    kotlin("jvm")
+    `maven-publish`
+    id("com.jfrog.bintray") version "1.8.4"
+}
+
+internal val theVersion = "1.0.0-SNAPSHOT"
+internal val theArtifactId = "mockk-junit5"
+internal val thePublication = "${theArtifactId}Publication"
+internal val theGroup = "com.github.erikhuizinga"
+
+group = theGroup
+version = theVersion
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.mockk:mockk:1.9.3")
+    implementation("org.junit.jupiter:junit-jupiter:5.6.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+java.withSourcesJar()
+
+publishing {
+    publications {
+        create<MavenPublication>(thePublication) {
+            groupId = theGroup
+            artifactId = theArtifactId
+            version = theVersion
+            from(components["java"])
+        }
+    }
+}
+
+configure<BintrayExtension> {
+    dryRun = true
+    user = properties["bintrayUser"] as String
+    key = properties["bintrayKey"] as String
+    pkg.apply {
+        version.apply {
+            name = theVersion
+            vcsTag = theVersion
+        }
+        repo = "maven"
+        name = theArtifactId
+        userOrg = "erikhuizinga"
+        setLicenses("Apache-2.0")
+        vcsUrl = "https://github.com/erikhuizinga/mockk-patterns.git"
+    }
+    setPublications(thePublication)
+}
