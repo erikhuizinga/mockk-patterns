d3408759e24557141ca2c62c0a018958687067a0
Erik Huizinga <erik.huizinga@nedap.com>
2020-03-20 22:25:24 +0100

    🔧 Config tests to exclude
    JUnit 4 test suites run test classes that otherwise run individually,
    which lead to double testing. By excluding the individual test class
    runs, this is prevented; the test suites still run the included classes.

--- junit4/build.gradle.kts
+++ junit4/build.gradle.kts
@@ -2,6 +2,13 @@ dependencies {
     implementation("junit:junit:$JUNIT4")
 }
 
+tasks.test {
+    exclude(
+        "**/*ExampleMockkTest*", // Runs in ExampleTestSuite
+        "**/*YourObjectIsNoLongerMockedTest*" // Runs in ExampleTestSuite
+    )
+}
+
 configurePublishing(
     version = "1.0.0",
     groupId = "com.github.erikhuizinga",
