95a16b6ff40a120cf08591c7e8b08ca160543c29
Erik Huizinga <erikhuizinga@users.noreply.github.com>
2021-02-16 14:23:46 +0100

    Configure publishing to OSSRH

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,6 +2,5 @@ plugins { `kotlin-dsl` }
 repositories { jcenter() }
 
 dependencies {
-    implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.70")
 }
--- junit5/build.gradle.kts
+++ junit5/build.gradle.kts
@@ -5,6 +5,7 @@ dependencies {
 tasks.test { useJUnitPlatform() }
 
 configurePublishing(
+    name = "MockK Patterns for JUnit 5",
     version = "2.0.0",
     artifactId = "mockk-junit5"
 )
